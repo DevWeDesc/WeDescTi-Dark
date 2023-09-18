0961ca42da5b4474a9a66e86f33a40779cf6a799
ViniHGV <ixxvinicius@gmail.com>
2023-09-18 14:03:36 -0300

    finalizando serviços

---
+++ src/Components/SwipperService.tsx
@@ -0,0 +1,170 @@
+"use client";
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCoverflow, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import Image from "next/image";
+import Logo from "@/image/Desc-Logo-Branco 1.png";
+import Arrow from "@/image/Vector (13).png";
+
+export const SwipperService = () => {
+  return (
+    <>
+      <Swiper
+        effect={"auto"}
+        grabCursor={true}
+        centeredSlides={false}
+        slidesPerView={4}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 100,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        spaceBetween={100}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide className="hover:scale-105 transition-all hover:scale-110 transition-all w-full bg-[#143654] rounded-3xl border-t-[26px] border-[#0088FF] my-10 ">
+          <div className="py-6 px-10 space-y-2">
+            <Image alt="" className=" object-scale-down" src={Logo} />
+            <h2 className="text-3xl font-bold">Titulo</h2>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industrys standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged.{" "}
+            </p>
+          </div>
+          <div className="py-2 mt-4 w-full border-t-2 border-white">
+            <a
+              href=""
+              className="min-w-full px-10 flex justify-between items-center hover:ml-2 transition-all brightness-150"
+            >
+              <strong>Mais sobre titulo</strong>
+              <Image
+                alt=""
+                className="h-4 w-3 object-contain"
+                src={Arrow}
+              ></Image>
+            </a>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="hover:scale-105 transition-all w-full bg-[#761B1B] rounded-3xl border-t-[26px] border-[#DC2626] my-10 ">
+          <div className="py-6 px-10 space-y-2">
+            <Image alt="" className=" object-scale-down" src={Logo} />
+            <h2 className="text-3xl font-bold">Titulo</h2>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industrys standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged.{" "}
+            </p>
+          </div>
+          <div className="py-2 mt-4 w-full border-t-2 border-white">
+            <a
+              href=""
+              className="min-w-full px-10 flex justify-between items-center hover:ml-2 transition-all brightness-150"
+            >
+              <strong>Mais sobre titulo</strong>
+              <Image
+                alt=""
+                className="h-4 w-3 object-contain"
+                src={Arrow}
+              ></Image>
+            </a>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="hover:scale-105 transition-all w-full bg-[#442054] rounded-3xl border-t-[26px] border-[#AB1BEE] my-10 ">
+          <div className="py-6 px-10 space-y-2">
+            <Image alt="" className=" object-scale-down" src={Logo} />
+            <h2 className="text-3xl font-bold">Titulo</h2>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industrys standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged.{" "}
+            </p>
+          </div>
+          <div className="py-2 mt-4 w-full border-t-2 border-white">
+            <a
+              href=""
+              className="min-w-full px-10 flex justify-between items-center hover:ml-2 transition-all brightness-150"
+            >
+              <strong>Mais sobre titulo</strong>
+              <Image
+                alt=""
+                className="h-4 w-3 object-contain"
+                src={Arrow}
+              ></Image>
+            </a>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="hover:scale-105 transition-all w-full bg-[#143654] rounded-3xl border-t-[26px] border-[#0088FF] my-10 ">
+          <div className="py-6 px-10 space-y-2">
+            <Image alt="" className=" object-scale-down" src={Logo} />
+            <h2 className="text-3xl font-bold">Titulo</h2>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industrys standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged.{" "}
+            </p>
+          </div>
+          <div className="py-2 mt-4 w-full border-t-2 border-white">
+            <a
+              href=""
+              className="min-w-full px-10 flex justify-between items-center hover:ml-2 transition-all brightness-150"
+            >
+              <strong>Mais sobre titulo</strong>
+              <Image
+                alt=""
+                className="h-4 w-3 object-contain"
+                src={Arrow}
+              ></Image>
+            </a>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="hover:scale-105 transition-all w-full bg-[#761B1B] rounded-3xl border-t-[26px] border-[#DC2626] my-10 ">
+          <div className="py-6 px-10 space-y-2">
+            <Image alt="" className=" object-scale-down" src={Logo} />
+            <h2 className="text-3xl font-bold">Titulo</h2>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industrys standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged.{" "}
+            </p>
+          </div>
+          <div className="py-2 mt-4 w-full border-t-2 border-white">
+            <a
+              href=""
+              className="min-w-full px-10 flex justify-between items-center hover:ml-2 transition-all brightness-150"
+            >
+              <strong>Mais sobre titulo</strong>
+              <Image
+                alt=""
+                className="h-4 w-3 object-contain"
+                src={Arrow}
+              ></Image>
+            </a>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import { Banner } from "@/Components/Banner";
 import { Header } from "@/Components/Header";
-import { Services } from "@/Components/Services";
 import { SubBanner } from "@/Components/SubBanner";
+import { SwipperService } from "@/Components/SwipperService";
 import Image from "next/image";
+import bgServices from "@/image/+.png";
 
 export default function Home() {
   return (
@@ -15,8 +16,13 @@ export default function Home() {
         <section>
           <SubBanner />
         </section>
-        <section>
-          <Services />
+        <section className="px-[70px] py-4 ">
+          <Image
+            src={bgServices}
+            className="absolute left-0 z-[-1] h-full w-full max-h-[720px]"
+            alt=""
+          />
+          <SwipperService />
         </section>
       </main>
     </>
