64d70fcbe0a6b6bea40aa6887959ca718112dbf9
ViniHGV <ixxvinicius@gmail.com>
2023-09-18 11:23:35 -0300

    Finalizando SubBanner

---
+++ src/Components/SubBanner.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const SubBanner = () => {
+  return (
+    <div className="py-24 grid pl-20 grid-cols-2 bg-gradient-to-r from-[#100515] to-[#290738]">
+      <div className="w-full h-full flex items-center justify-center">
+        <p className="m-0 py-20 text-2xl max-w-[350px] leading-relaxed font-bold">
+          Nossa paixão é oferecer soluções tecnológicas inovadoras que
+          impulsionam o crescimento e otimizam a eficiência do seu negócio.
+        </p>
+      </div>
+      <div>
+        <div className="w-[80%] h-[100%] bg-black border-[1px] border-[#0088FF]"></div>
+      </div>
+    </div>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { Banner } from "@/Components/Banner";
 import { Header } from "@/Components/Header";
+import { Services } from "@/Components/Services";
+import { SubBanner } from "@/Components/SubBanner";
 import Image from "next/image";
 
 export default function Home() {
@@ -9,6 +11,12 @@ export default function Home() {
       <main className="flex flex-col w-full">
         <section className="pl-20 flex items-center justify-center">
           <Banner />
+        </section>
+        <section>
+          <SubBanner />
+        </section>
+        <section>
+          <Services />
         </section>
       </main>
     </>
